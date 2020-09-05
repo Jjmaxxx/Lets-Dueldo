5e07f2bc3ae59fea7326525afead80604958fc19
Jjmaxxx <justlee888@gmail.com>
2020-09-04 19:03:38 -0700

    game states

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ let handleRequest = (request,response)=>{
         pathName = "/index.html";
     }
     let extension = path.extname(pathName);
+
     const typeExtension = {
         ".html":"text/html",
         ".js":"text/javascript",
         "css":"text/css"
     }
+
     //if file type spits out empty string/doesnt match file types in dictionary pick text/plain
     let contentType = typeExtension[extension] || "text/plain";
     fs.readFile(__dirname + pathName,(error,data)=>{
@@ -28,14 +30,33 @@ let handleRequest = (request,response)=>{
 };
 const server = http.createServer(handleRequest);
 server.listen(3000);
+
+//create a list of data objects that represents the order of keystrokes sent by users
+let gameState = [
+
+];
+
+let users = [];
+
+
+
 let io = require("socket.io").listen(server);
 io.sockets.on('connection', (socket) =>{
     console.log('client connected with ' + socket.id);
+    users.push(socket.id);
+    socket.emit('join',(gameState));
+    console.log(users);
+
     socket.on("disconnect", ()=>{
-        console.log( socket.id + " has disconnected");
+        console.log(socket.id + " has disconnected");
+        users.splice(users.indexOf(socket.id),1);
+        console.log(users);
     })
+
+
     socket.on('mouse', (data)=>{
         console.log('recieved mouse' + data.x + " " + data.y);
+        gameState.push(data);
         //broadcast gives it to all nodes except for the node that called it
         socket.broadcast.emit('mouse', data);
     })
--- sketch.js
+++ sketch.js
@@ -8,11 +8,32 @@ function setup(){
         noStroke();
         ellipse(data.x,data.y,80,80);
     })
+    
+    socket.on('join',(gameState)=>{
+        fill('blue');
+        noStroke();
+        for(let i=0; i<gameState.length; i++){
+            ellipse(gameState[i].x,gameState[i].y,80,80);
+        }
+    });
+
+    //just an event to toggle the turn on or off /
+    //send the id of the socket whos turn it is to every user, then the client can figure out if its their turn
+    //socket.on("toggleTurn",isMyturn) => {myTurn = isMyturn}
+    //git branch nbame
+
+
+
 }
 function draw(){
 
 }
 function mouseDragged(){
+
+    // if(IsMyTurn){
+    //     //do the stuff
+    // }
+
     let data = {
         x: mouseX,
         y: mouseY
