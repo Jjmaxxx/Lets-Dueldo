bd1950fd793d9101ba836332321a10c00a582a5d
Jjmaxxx <justlee888@gmail.com>
2020-09-11 19:04:32 -0700

    updated

--- index.js
+++ index.js
@@ -30,23 +30,65 @@ let handleRequest = (request,response)=>{
 };
 const server = http.createServer(handleRequest);
 server.listen(3000);
+console.log("running on port 3000");
 
 //create a list of data objects that represents the order of keystrokes sent by users
 let gameState = [
+    
+];
+let gameStateP1 = [
+
+];
+let gameStateP2 = [
 
 ];
 
+let prompts = ["cat","dog","parrot","elephant","giraffe"];
 let users = [];
-
-
-
+let randNum;
+let player1;
+let p1Votes = 0;
+let p2Votes = 0;
 let io = require("socket.io").listen(server);
 io.sockets.on('connection', (socket) =>{
     console.log('client connected with ' + socket.id);
     users.push(socket.id);
     socket.emit('join',(gameState));
     console.log(users);
-
+    socket.on("newRound",()=>{
+        p1Votes = 0;
+        p2Votes = 0;
+        randNum = users[Math.floor(Math.random(0, users.length))];
+        if(users.length >= 2){
+            io.to(randNum).emit('playerNumber', 1);
+            player1 = randNum;
+            randNum = users[Math.round(Math.random(0, users.length))];
+            while(randNum == player1){
+                randNum = users[Math.floor(Math.random(0, users.length))];
+            } 
+            io.to(randNum).emit('playerNumber', 2);
+        }
+        let timer = 1;
+        let timerInterval = setInterval(function(){
+            timer--;
+            io.emit('timer', timer);
+            if(timer <= 0){
+                clearInterval(timerInterval);
+                io.emit('voting');
+            }
+        },1000)
+        io.emit('newRound', prompts[Math.floor(Math.random(0,prompts.length))]);
+    })
+    socket.on('vote',(player)=>{
+        if(player == 1){
+            p1Votes++;
+        }
+        else if (player == 2){
+            p2Votes++;
+        }
+        console.log(p2Votes);
+        io.emit('vote',[p1Votes,p2Votes]);
+    })
     socket.on("disconnect", ()=>{
         console.log(socket.id + " has disconnected");
         users.splice(users.indexOf(socket.id),1);
@@ -55,9 +97,12 @@ io.sockets.on('connection', (socket) =>{
 
 
     socket.on('mouse', (data)=>{
-        console.log('recieved mouse' + data.x + " " + data.y);
+        //console.log('recieved mouse' + data.x + " " + data.y);
         gameState.push(data);
         //broadcast gives it to all nodes except for the node that called it
         socket.broadcast.emit('mouse', data);
+    })
+    socket.on('clear', (num)=>{
+        socket.broadcast.emit('clear', num);
     })
 })
--- sketch.js
+++ sketch.js
@@ -1,14 +1,60 @@
 let socket;
+let playerNumber;
+let timer;
+let p1Total,p2Total;
 function setup(){
+    frameRate(60);
     background(0);
-    createCanvas(windowWidth, windowHeight);
+    let canvas = createCanvas(windowWidth, windowHeight);
+    startGame = createButton("Start Game");
+    startGame.position(windowWidth/2,windowHeight/2);
+    startGame.mousePressed(function(){socket.emit('newRound');startGame.remove()});
     socket = io.connect("http://localhost:3000");
     socket.on('mouse', (data)=>{
-        fill('blue');
+        if(data.playerNum == 1){
+            push();
+            fill('blue');
+            ellipse(data.x,data.y,80,80);
+            pop();
+        }else if(data.playerNum == 2){
+            push();
+            fill('red');
+            ellipse(data.x,data.y,80,80);
+            pop();
+        }
         noStroke();
-        ellipse(data.x,data.y,80,80);
     })
-    
+    socket.on("newRound",(prompt)=>{
+        startGame.remove();
+        text(prompt, windowWidth/2, 20);
+    })
+    socket.on('voting',()=>{
+        voteP1 = createButton("vote");
+        voteP1.position(windowWidth/4, windowHeight-100);
+        voteP1.mousePressed(function(){socket.emit("vote", 1)});
+        voteP2 = createButton("vote");
+        voteP2.position(windowWidth * 3/4, windowHeight-100);
+        voteP2.mousePressed(function(){socket.emit("vote", 2)});
+    })
+    socket.on('vote',(votes)=>{
+        if(p1Total == null){
+            p1Total = createDiv("Votes: " + votes[0]);
+            p1Total.position(windowWidth/4, windowHeight-50);
+        }
+        if(p2Total == null){
+            p2Total = createDiv("Votes: " + votes[1]);
+            p2Total.position(windowWidth * 3/4, windowHeight-50);
+        }
+        p1Total.html("Votes: "+votes[0]);
+        p2Total.html("Votes: "+votes[1]);
+    })
+    socket.on("timer",(timeLeft)=>{
+        if(timer == null){
+            timer = createDiv("Time Left: " + timeLeft, 100, 100);
+            timer.position(10,10);
+        }
+        timer.html("Time Left: "+timeLeft);
+    })
     socket.on('join',(gameState)=>{
         fill('blue');
         noStroke();
@@ -16,30 +62,74 @@ function setup(){
             ellipse(gameState[i].x,gameState[i].y,80,80);
         }
     });
-
+    socket.on('clear',(num)=>{
+        if(num == 1){
+            fill('white'); 
+            rect(0, 0, windowWidth/2 - 2,windowHeight);
+            noStroke();
+        }else{
+            fill('white'); 
+            rect(windowWidth/2 + 2, 0, windowWidth/2, windowHeight); 
+            noStroke();
+        }
+    })
+    socket.on('playerNumber', (num)=>{
+        playerNumber = num;
+        console.log("I am player: " + playerNumber);
+    });
+    clearBoardP1 = createButton("Clear");
+    clearBoardP1.position(windowWidth/2 - 90, 0);
+    clearBoardP1.mousePressed(function(){if(playerNumber == 1){fill('white'); rect(0, 0, windowWidth/2 - 2,windowHeight); noStroke();socket.emit("clear", 1)}})
+    clearBoardP2 = createButton("Clear");
+    clearBoardP2.position(windowWidth/2 + 50, 0);
+    clearBoardP2.mousePressed(function(){if(playerNumber == 2){fill('white'); rect(windowWidth/2 + 2, 0, windowWidth/2, windowHeight); noStroke();socket.emit("clear", 2)}});
     //just an event to toggle the turn on or off /
     //send the id of the socket whos turn it is to every user, then the client can figure out if its their turn
     //socket.on("toggleTurn",isMyturn) => {myTurn = isMyturn}
     //git branch nbame
 
 
-
+    
 }
 function draw(){
-
+    line(windowWidth/2, windowHeight, windowWidth/2, -windowHeight);
 }
 function mouseDragged(){
-
-    // if(IsMyTurn){
-    //     //do the stuff
-    // }
-
     let data = {
         x: mouseX,
-        y: mouseY
+        y: mouseY,
+        playerNum: playerNumber
     }
-    fill('blue');
-    noStroke();
-    ellipse(data.x,data.y,80,80);
-    socket.emit("mouse", data);
+    if(playerNumber == 1){
+        if(data.x < windowWidth/2){
+            push();
+            fill('blue');
+            noStroke();
+            ellipse(data.x,data.y,80,80);
+            pop();
+            socket.emit("mouse", data);
+        }
+    }
+    else if(playerNumber == 2){
+        if(data.x > windowWidth/2){
+            push();
+            fill('red');
+            noStroke();
+            ellipse(data.x,data.y,80,80);
+            pop();
+            socket.emit("mouse", data);
+        }
+    }
 }
+// function countDown(){
+//     while(timer >= 0){
+//         console.log('a');
+//         text("Time Left: " + timer, 0,0);
+//         if(frameCount % 60 == 0){
+//             timer --;
+//         }
+//     }
+//     if(timer < 0){
+//         socket.emit('newRound');
+//     }
+// }
